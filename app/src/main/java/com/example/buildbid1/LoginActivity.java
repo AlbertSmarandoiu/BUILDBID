55846475bd73ef01c33dbb6e8db8666405b08415
Madalin Dumitru <72272083+hiddenache@users.noreply.github.com>
2024-10-12 15:29:23 +0300

    change db name (#1)
    Co-authored-by: hiddenache <hidden.madalin@gmail.com>

--- app/src/main/java/com/example/buildbid1/LoginActivity.java
+++ app/src/main/java/com/example/buildbid1/LoginActivity.java
@@ -18,9 +18,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText edUsername , edPassword;
-    Button btnLogare , btnInregistrare;
-    TextView tvLogo , tvLogin , tvDacaNuAiCont;
+    EditText edUsername, edPassword;
+    Button btnLogare, btnInregistrare;
+    TextView tvLogo, tvLogin, tvDacaNuAiCont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,42 +28,41 @@ public class LoginActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_login);
 
-            edUsername = findViewById(R.id.editTextUsernameLogin);
-            edPassword = findViewById(R.id.editTextLoginParola);
-            btnLogare = findViewById(R.id.buttonLogin);
-            btnInregistrare = findViewById(R.id.buttonregister);
-            tvLogo = findViewById(R.id.textLogo);
-            tvLogin = findViewById(R.id.textLoginTitle);
-            tvDacaNuAiCont = findViewById(R.id.textDacaNuAiCont);
-        Database db = new Database(getApplicationContext(),"healtcare" , null , 1);
-            btnLogare.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String username = edUsername.getText().toString();
-                    String password = edPassword.getText().toString();
-                    if(username.length() == 0 || password.length() == 0) {
-                        Toast.makeText(getApplicationContext(), "Va rog sa introduceti in campul nume", Toast.LENGTH_SHORT).show();
+        edUsername = findViewById(R.id.editTextUsernameLogin);
+        edPassword = findViewById(R.id.editTextLoginParola);
+        btnLogare = findViewById(R.id.buttonLogin);
+        btnInregistrare = findViewById(R.id.buttonregister);
+        tvLogo = findViewById(R.id.textLogo);
+        tvLogin = findViewById(R.id.textLoginTitle);
+        tvDacaNuAiCont = findViewById(R.id.textDacaNuAiCont);
+        Database db = new Database(getApplicationContext(), "buildBid_db", null, 1);
+        btnLogare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                if (username.length() == 0 || password.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Va rog sa introduceti in campul nume", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (db.login(username, password) == 1) {
+                        Toast.makeText(getApplicationContext(), "Logare cu Succes", Toast.LENGTH_SHORT).show();
+                        SharedPreferences sharedpreferences = getSharedPreferences("shared_pref", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedpreferences.edit();
+                        editor.putString("username", username);
+                        editor.apply();
+                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Logare nu a reusit cu succes ", Toast.LENGTH_SHORT).show();
                     }
-                    else {
-                        if (db.login(username, password) == 1) {
-                            Toast.makeText(getApplicationContext(), "Logare cu Succes", Toast.LENGTH_SHORT).show();
-                            SharedPreferences sharedpreferences = getSharedPreferences("shared_pref" , Context.MODE_PRIVATE);
-                            SharedPreferences.Editor editor = sharedpreferences.edit();
-                            editor.putString("username" , username);
-                            editor.apply();
-                            startActivity(new Intent(LoginActivity.this,HomeActivity.class));
-                        }else{
-                            Toast.makeText(getApplicationContext(), "Logare nu a reusit cu succes ", Toast.LENGTH_SHORT).show();
-                        }
 
-                    }
                 }
-            });
-            btnInregistrare.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
-                }
-            });
+            }
+        });
+        btnInregistrare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
